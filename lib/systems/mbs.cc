080c292b09abee06296a9961a592ac61b128bdb8
Marin Kobilarov <mkobila1@jhu.edu>
2014-01-03 16:44:50 -0500

    moved Bias

--- lib/systems/mbs.cc
+++ lib/systems/mbs.cc
@@ -160,12 +160,10 @@ void Mbs::FK(MbsState &x)
 }
 
 
-
-
 void Mbs::ID(VectorXd &f,
              double t, const MbsState &x, const VectorXd &u) 
 {
-  VectorXd b(nb+5); // bias
+  VectorXd b(nb + 5); // bias
   Bias(b, t, x);
 
   VectorXd fu(nb + 5);
@@ -180,7 +178,7 @@ void Mbs::ID(VectorXd &f,
 
 void Mbs::Bias(VectorXd &b,
                double t,
-               const MbsState &x) const 
+               const MbsState &x) const
 {
   
   // assume that Kstep was called so that all velocities are propagated
