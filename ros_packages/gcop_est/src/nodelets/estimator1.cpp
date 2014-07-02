2bf9cb2de94413f66313ea83e9d946b6c2a8439d
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2014-07-02 16:53:00 -0400

    Moved camera files from utils to estimation folder

--- ros_packages/gcop_est/src/nodelets/estimator1.cpp
+++ ros_packages/gcop_est/src/nodelets/estimator1.cpp
@@ -24,6 +24,7 @@ namespace gcop_est {
 
 		//boost::mutex connect_mutex_;
 
+		image_transport::Publisher pub_cont_;
 		//ros::Publisher pose_publisher;
 		bool initcamera;
 		int length_object;
@@ -36,6 +37,7 @@ namespace gcop_est {
 
 		//Member Functions:
 		virtual void onInit();
+		//tf::Transform result_transform;
 		//void connectCb();
 		void imageCb(const sensor_msgs::ImageConstPtr& image_msg,
 				const sensor_msgs::CameraInfoConstPtr& info_msg);
@@ -52,6 +54,7 @@ namespace gcop_est {
 		//image_transport::SubscriberStatusCallback connect_cb = boost::bind(&RectifyNodelet::connectCb, this);
 		image_transport::TransportHints hints("raw", ros::TransportHints(), private_nh);
 		sub_camera_ = it_->subscribeCamera("image", 1, &PoseEstimator1::imageCb, this, hints);
+		pub_cont_  = it_->advertise("image_cont",  1);//Contour image
 	}
 
 	// Handles (un)subscribing when clients (un)subscribe
@@ -79,15 +82,29 @@ namespace gcop_est {
 		}
 		if(!initcamera)
 		{
-			cam.reset(new gcop::Camera(length_object,info_msg->height,info_msg->width));
+			assert(info_msg->D.size() == 5);//Ensure that there are 5 distortion coefficients
+			cam.reset(new gcop::Camera(length_object,info_msg->height,info_msg->width,&info_msg->K[0],&info_msg->D[0]));//Assuming the plum bob model for camera distortion
 			initcamera = true;//Camera class is initialized
+			//Get parameters for  blurring kernel size and tolerance for radius search:
+			double camtolerance;
+			if(ros::param::get("/camera/tolerance_radius",camtolerance))
+			{
+				cam->tolerance = (float)camtolerance;
+				cout<<cam->tolerance<<"-------"<<endl;
+			}
+			ros::param::get("/camera/kernel_blur",cam->kernel_size);
 		}
 		// Create cv::Mat views onto both buffers
 		const cv::Mat image = cv_bridge::toCvShare(image_msg)->image;
+		cv::Mat contourImage;
 		if(cam)//If cam is initialized
 		{
 			//Use the camera library to process for pose
-			bool ok = cam->Pose(q,image);//Find the pose
+			bool ok = cam->Pose(q,image,contourImage);//Find the pose and get a contour image
+			// Allocate new contour message
+			//sensor_msgs::ImagePtr cont_msg = cv_bridge::CvImage(image_msg->header, image_msg->encoding, contourImage).toImageMsg();
+			sensor_msgs::ImagePtr cont_msg = cv_bridge::CvImage(image_msg->header, "8UC3", contourImage).toImageMsg();
+			pub_cont_.publish(cont_msg);
 			if (!ok) {
 				ROS_WARN("No Object");
 				return;
@@ -95,17 +112,19 @@ namespace gcop_est {
 			//xyz =q.tail<3>();//First three are posn
 			SO3::Instance().exp(g,q.head<3>());//Convert the exponential coords to matrix
 			SO3::Instance().g2quat(wxyz,g);//Convert g to rpy
+			//tf::Vector3 changeinposn = result_transform.getOrigin() - tf::Vector3(q(3),q(4),q(5));
+			//cout<<"Change in posn between two frames: "<< changeinposn.x()<<"\t"<<changeinposn.y()<<"\t"<<changeinposn.z()<<"\t"<<endl;
 			result_transform.setOrigin(tf::Vector3(q(3),q(4),q(5)));
 			result_transform.setRotation(tf::Quaternion(wxyz(1),wxyz(2),wxyz(3),wxyz(0)));
 			//br.sendTransform(tf::StampedTransform(result_transform,image_msg->header.stamp,"camera","object"));//transform from camera(parent) frame to object(child) frame
 			br.sendTransform(tf::StampedTransform(result_transform,ros::Time::now(),"camera","object"));//transform from camera(parent) frame to object(child) frame
 			/*
-			cout<<"Q output: "<<q.transpose()<<endl;
+				 cout<<"Q output: "<<q.transpose()<<endl;
 
-			cout << "rpy=" << rpy.transpose() << "xyz=" << xyz.transpose() << endl;
+				 cout << "rpy=" << rpy.transpose() << "xyz=" << xyz.transpose() << endl;
 
-			cout << "x=" << -xyz[:2] << " y=" << xyz[0] << " yaw=" << -rpy[0]*(180.0/M_PI) << endl;//Will move this into tf publishing TODO
-			*/
+				 cout << "x=" << -xyz[:2] << " y=" << xyz[0] << " yaw=" << -rpy[0]*(180.0/M_PI) << endl;//Will move this into tf publishing TODO
+			 */
 		}
 	}
 }//namespace gcop_est
