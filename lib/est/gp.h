6ebfe24aad30422db2f56c278dee0dd835adf937
mkobila1 <mkobila1@jhu.edu>
2015-03-22 22:14:01 -0400

    added documentation to GP

--- lib/est/gp.h
+++ lib/est/gp.h
@@ -1,3 +1,11 @@
+// This file is part of libgcop, a library for Geometric Control, Optimization, and Planning (GCOP)
+//
+// Copyright (C) 2004-2014 Marin Kobilarov <marin(at)jhu.edu>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #ifndef GCOP_GP_H
 #define GCOP_GP_H
 
@@ -6,39 +14,88 @@
 namespace gcop {
   class GP {
   public:
-    GP(int d, int n);
+    /**
+     * Initialize a GP with dimension d and number of points n
+     * @param d dimension
+     * @param n number of points (optional)
+     */
+    GP(int d, int n = 0);
     
     virtual ~GP();
     
+    /**
+     * Generate random points
+     */ 
     virtual void Sample();
     
+    /**
+     * Add a new data point
+     * @param x data vector
+     * @param f value
+     * @param true if OK
+     */
     bool Add(const Eigen::VectorXd &x,
              double f);    
     
+    /**
+     * Train using current data
+     */
     void Train();
     
+    /**
+     * Train GP using a given dataset (xs, fs)
+     * @param d-n matrix of data vectors
+     * @param n-vector of values
+     */
     void Train(const Eigen::MatrixXd &Xs, 
                const Eigen::VectorXd &fs);
     
+    /**
+     * Predict value at point x
+     * @param x point
+     * @param s pointer to predicted covariance (optional)
+     * @return predicted mean
+     */
     double Predict(const Eigen::VectorXd &x, 
                    double *s = 0) const;
     
+    /**
+     * Square exponential kernel
+     * @param xa first point
+     * @param xb second point
+     * @return correlation
+     */
     double SqExp(const Eigen::VectorXd &xa, 
                  const Eigen::VectorXd &xb) const;
     
+    /**
+     * Loglikelihood
+     * @param dll derivative of log-liklihood w.r. to l and s
+     * @return log-likelihood
+     */
     double LogL(double dll[2] = 0);
 
+    /**
+     * Probability of improvement over a given value fmin
+     * @aram x data vector
+     * @param fmin baseline
+     * @return probability of improvement
+     */
     double PI(const Eigen::VectorXd &x, double fmin) const;
 
+    /**
+     * Optimize GP parameters. This is currently done naively
+     * using a grid enumeration over l and s
+     */
     double OptParams();
+       
+    int d;  ///< dimension
+    int n;  ///< number of data points
     
-    int d;
-    int n;
+    Eigen::MatrixXd Xs;   ///< data points
+    Eigen::VectorXd fs;   ///< values
     
-    Eigen::MatrixXd Xs;
-    Eigen::VectorXd fs;
-    
-    Eigen::MatrixXd K;
+    Eigen::MatrixXd K;  
     Eigen::MatrixXd Ki;
     
     Eigen::MatrixXd L;
