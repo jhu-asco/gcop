f1594f84c6eadb90e990d6f886b6cfc2ac531155
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2015-05-13 22:00:40 -0400

    Extended rename.limit for git in applydocs

--- applydocs.bash
+++ applydocs.bash
@@ -3,5 +3,5 @@ git add -f docs
 git stash 
 git checkout gh-pages
 git stash apply
-grep -lr '<<<<<<<' . | xargs git checkout --theirs
+grep -lr '<<<<<<< ' . | xargs git checkout --theirs
 git add -u .
