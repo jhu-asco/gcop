fbb6b6eddc09c5157c332f483724ba6fc0dd86d9
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2015-05-13 22:19:15 -0400

    Extended rename limit for git

--- applydocs.bash
+++ applydocs.bash
@@ -1,7 +1,9 @@
 doxygen
+git config merge.renameLimit 999999
 git add -f docs
 git stash 
 git checkout gh-pages
 git stash apply
 grep -lr '<<<<<<< ' . | xargs git checkout --theirs
 git add -u .
+git config --unset merge.renameLimit
