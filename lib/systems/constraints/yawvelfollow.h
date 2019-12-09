a0b97aaf95f86333cbc347b793715d9378f6f561
gabe <gbaraba1@jhu.edu>
2019-12-09 18:16:27 -0500

    yaw cost (1-df*Re)

--- lib/systems/constraints/yawvelfollow.h
+++ lib/systems/constraints/yawvelfollow.h
@@ -1,5 +1,5 @@
-#ifndef GCOP_GROUNDPLANE_H
-#define GCOP_GROUNDPLANE_H
+#ifndef GCOP_YAWVELFOLLOW_H
+#define GCOP_YAWVELFOLLOW_H
 
 #include "constraint.h"
 #include <math.h>
@@ -53,15 +53,22 @@ namespace gcop {
       const Vector3d &vel = x.v; // velocity
       const Matrix3d &Rot = x.R; // Rotation Matrix
      
-      double yaw = atan2(Rot(1,0),Rot(0,0));//Current yaw
+      double yaw = atan2(Rot(1,0),Rot(0,0));//Yaw Based on SO3 Code
+      //NOTE: If both arguments are 0, yaw is undefined, but atan2 returns 0
       double yaw_d = atan2(vel[1],vel[0]);//Velocity following yaw
-
-      double d = yaw - yaw_d;  // distance between angles
-      if (d > M_PI){
-        d -= 2*M_PI;
-      }
-      if (d < -M_PI){
-        d += 2*M_PI;
+      
+      double vsq = vel[1]*vel[1] + vel[0]*vel[0]; //Velocity magnitude
+      double d;
+      if (vsq < 1e-10) { //At small velocity, constraint is ill-defined
+        d = 0;
+      } else {
+        double d = yaw - yaw_d;  // distance between angles
+        if (d > M_PI){
+          d -= 2*M_PI;
+        }
+        if (d < -M_PI){
+          d += 2*M_PI;
+        }
       }
 
       g[0] = d*d; // must be 0 for non-collision
@@ -71,21 +78,31 @@ namespace gcop {
         //THIS IS A BUG.  For states that are not Body3dState, it won't necessarily work.
         //It assumes that the position indices of the x vector are 3,4,5, but other states use 0,1,2
         //DO NOT TRUST 
-        //0,1,2 are log(R), 9,10,11 are velocity
-        double partial_dx = 2*d;
+        if ((abs(d) < 1e-10)||(vsq < 1e-10)) {//Derivative is 0
+          return true;
+        }
+        double R1 = Rot(1,0);
+        double R0 = Rot(0,0);
+        double R_sq = R0*R0 + R1*R1;
+        if (R_sq < 1e-10) {//Derivative is 0
+          return true;
+        }
         Vector3d eR;
         SO3::Instance().log(eR,Rot);
         double theta = eR.norm();
-        Vector3d dthetadeR = eR/theta;
-        double R_squared = Rot(0,0)*Rot(0,0) + Rot(1,0)*Rot(1,0);
-	Vector3d temp = Vector3d(0, 2*eR[1], 2*eR[2]);
-        Vector3d dR00deR = (eR[1]*eR[1] + eR[2]*eR[2])*(-sin(theta)*dthetadeR) + (cos(theta)-1)*temp;
-        Vector3d temp2(-eR[1]*(cos(theta)-1), -eR[0]*(cos(theta) -1), sin(theta));
-        Vector3d dR10deR = (eR[2]*cos(theta) + eR[0]*eR[1]*sin(theta))*dthetadeR + temp2;
-        dgdx->row(0).head(3) = partial_dx*((Rot(0,0)/R_squared)*dR10deR - (Rot(1,0)/R_squared)*dR00deR);
-        double vel_squared = vel[0]*vel[0] + vel[1]*vel[1];
-        dgdx->row(0)[9] = partial_dx*(vel[1]/vel_squared);
-        dgdx->row(0)[10] = partial_dx*(-vel[0]/vel_squared);
+        if (theta < 1e-10) {//Derivative is 0
+          return true;
+        }
+        double partial_dx = 2*d;
+        Vector3d dThetadeR = eR/theta;
+        Vector3d temp(0,2*eR[1],2*eR[2]);
+        Vector3d dR0deR = (eR[1]*eR[1] + eR[2]*eR[2])*(-sin(theta))*dThetadeR + (cos(theta)-1)*temp;
+        Vector3d temp2(-eR[1]*(cos(theta)-1),-eR[0]*(cos(theta)-1),sin(theta));
+        Vector3d dR1deR = eR[2]*cos(theta)*dThetadeR + temp2 + eR[0]*eR[1]*sin(theta)*dThetadeR;
+        //0,1,2 are log(R), 9,10,11 are velocity
+        dgdx->row(0).head(3) = partial_dx*((R0/R_sq)*dR1deR - (R1/R_sq)*dR0deR);
+        dgdx->row(0)[9] = partial_dx*(vel[1]/vsq);
+        dgdx->row(0)[10] = partial_dx*(-vel[0]/vsq);
       }
     }
 };
